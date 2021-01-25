2d1191f3363a72cb4c6cffcd079631e289e06b7d
nikitakuznetsoff <nikitakuznetsov86@gmail.com>
2021-01-26 02:30:50 +0300

    array - easy tasks

--- Easy/Array/283.py
+++ Easy/Array/283.py
@@ -7,4 +7,21 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        test = 1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero_ind = 0
+        ind = 1
+
+        while ind < len(nums) and zero_ind < len(nums):
+            while zero_ind < len(nums) and nums[zero_ind] != 0:
+                zero_ind += 1
+
+            if ind < zero_ind:
+                ind = zero_ind
+
+            while ind < len(nums) and nums[ind] == 0:
+                ind += 1
+
+            if zero_ind < len(nums) and ind < len(nums):
+                nums[ind], nums[zero_ind] = nums[zero_ind], nums[ind]
